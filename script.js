fd033bf50291da621cd8b4634149792c7a69060c
Andre Agung Purnomo <andrepurnomo04@gmail.com>
2020-04-03 00:55:41 +0700

    update pindah ke link biasa

--- script.js
+++ script.js
@@ -1,10 +1,15 @@
+$(".urlButton").hide();
+
 function download() {
   url = $("input[name=url]").val() + "&__a=1";
 
   $.getJSON(url, {}).done(function(data) {
     media = data["graphql"]["shortcode_media"];
 
-    if (media.is_video) window.open(media.video_url, "_blank");
-    else window.open(media.display_url, "_blank");
+    if (media.is_video) urlDownload = video_url;
+    else urlDownload = media.display_url;
+
+    $(".urlButton").attr("href", urlDownload);
+    $(".urlButton").show();
   });
 }
