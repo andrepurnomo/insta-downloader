0b52182c96fcb10fa6ad39460f3fb95a616afdbb
Andre Agung Purnomo <andrepurnomo04@gmail.com>
2020-04-03 01:06:30 +0700

    fix video download

--- script.js
+++ script.js
@@ -1,12 +1,13 @@
 $(".urlButton").hide();
 
 function download() {
+  $(".urlButton").hide();
   url = $("input[name=url]").val() + "&__a=1";
 
   $.getJSON(url, {}).done(function(data) {
     media = data["graphql"]["shortcode_media"];
 
-    if (media.is_video) urlDownload = video_url;
+    if (media.is_video) urlDownload = media.video_url;
     else urlDownload = media.display_url;
 
     $(".urlButton").attr("href", urlDownload);
