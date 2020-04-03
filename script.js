118efae05a1a1bd8cf3ae10a15a238e67770448b
Andre Agung Purnomo <andrepurnomo04@gmail.com>
2020-04-03 09:16:13 +0700

    tambah reset dan tambah fail catch

--- script.js
+++ script.js
@@ -1,16 +1,28 @@
 $(".urlButton").hide();
+$(".urlReset").hide();
 
 function download() {
   $(".urlButton").hide();
   url = $("input[name=url]").val() + "&__a=1";
 
-  $.getJSON(url, {}).done(function(data) {
-    media = data["graphql"]["shortcode_media"];
+  $.getJSON(url, {})
+    .done(function(data) {
+      media = data["graphql"]["shortcode_media"];
 
-    if (media.is_video) urlDownload = media.video_url;
-    else urlDownload = media.display_url;
+      if (media.is_video) urlDownload = media.video_url;
+      else urlDownload = media.display_url;
 
-    $(".urlButton").attr("href", urlDownload);
-    $(".urlButton").show();
-  });
+      $(".urlButton").attr("href", urlDownload);
+      $(".urlButton").show();
+      $(".urlReset").show();
+    })
+    .fail(function() {
+      alert("Url not found, please try copy a valid link.");
+    });
+}
+
+function reset() {
+  $(".urlButton").hide();
+  $(".urlReset").hide();
+  $("input[name=url]").val("");
 }
