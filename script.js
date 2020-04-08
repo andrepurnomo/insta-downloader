597c087467d3170d62108709c7aea9c43c527155
Andre Agung Purnomo <andrepurnomo04@gmail.com>
2020-04-08 21:40:21 +0700

    ubah html5 download

--- script.js
+++ script.js
@@ -1,28 +1,26 @@
 $(".urlButton").hide();
-$(".urlReset").hide();
 
 function download() {
   $(".urlButton").hide();
   url = $("input[name=url]").val() + "&__a=1";
 
   $.getJSON(url, {})
-    .done(function(data) {
+    .done(function (data) {
       media = data["graphql"]["shortcode_media"];
 
       if (media.is_video) urlDownload = media.video_url;
       else urlDownload = media.display_url;
 
       $(".urlButton").attr("href", urlDownload);
+      $(".urlButton").attr("download", "");
       $(".urlButton").show();
-      $(".urlReset").show();
     })
-    .fail(function() {
+    .fail(function () {
       alert("Url not found, please try copy a valid link.");
     });
 }
 
 function reset() {
   $(".urlButton").hide();
-  $(".urlReset").hide();
   $("input[name=url]").val("");
 }
